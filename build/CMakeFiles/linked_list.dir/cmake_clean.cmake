a0198e5a4a1064467c75a26fe707728d49943f5a
pepohta <pedroh.tandreazza@gmail.com>
2026-03-17 17:23:53 -0300

    Finalização do exercício de linked list

---
+++ build/CMakeFiles/linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linked_list.dir/main.cpp.obj"
+  "CMakeFiles/linked_list.dir/main.cpp.obj.d"
+  "CMakeFiles/linked_list.dir/src/linked_list.cpp.obj"
+  "CMakeFiles/linked_list.dir/src/linked_list.cpp.obj.d"
+  "liblinked_list.dll.a"
+  "linked_list.exe"
+  "linked_list.exe.manifest"
+  "linked_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/src/linked_list.cpp.obj"
+  "CMakeFiles/tests.dir/src/linked_list.cpp.obj.d"
+  "CMakeFiles/tests.dir/tests/tests.cpp.obj"
+  "CMakeFiles/tests.dir/tests/tests.cpp.obj.d"
+  "libtests.dll.a"
+  "tests.exe"
+  "tests.exe.manifest"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
